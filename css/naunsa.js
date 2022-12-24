f8ec2be9b04861a4add81c1316e617145fb36f5b
darren1234x <114471474+darren1234x@users.noreply.github.com>
2022-12-25 02:58:12 +0800

    Initial commit

---
+++ css/naunsa.js
@@ -0,0 +1,11 @@
+var typed = new Typed(".styling", {
+    strings: [ "STUDENT.", "BEGINNER."],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});var typed = new Typed(".typing", {
+    strings: [ "WEBSITE!", "PORTFOLIO!"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
