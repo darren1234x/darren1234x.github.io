7d037ffa3bf33f22cdf199fc740c420ec622d2ff
darren1234x <114471474+darren1234x@users.noreply.github.com>
2022-12-27 19:58:15 +0800

    Initial commit

--- js/naunsa.js
+++ js/naunsa.js
@@ -1,4 +1,3 @@
-
 var typed = new Typed(".styling", {
     strings: [ "STUDENT.", "BEGINNER."],
     typeSpeed: 100,
@@ -10,7 +9,7 @@ var typed = new Typed(".styling", {
     backSpeed: 60,
     loop: true
 });
-/*Skills*/
+//Skills
 const skillItems = document.querySelectorAll('.skill-item');
 
 skillItems.forEach(item => {
@@ -26,6 +25,25 @@ skillItems.forEach(item => {
     this.classList.remove('active');
   });
 });
+
+//Projects
+$(document).on('click', '.project-filter li', function(){
+  $(this) .addClass('project-filter-active').siblings ().removeClass('project-filter-active')
+});
+
+$(document).ready(function(){
+  $('.list').click(function(){
+      const value = $(this).attr('data-filter');
+      if(value == 'all'){
+          $('.project-box').show('1000');
+      }
+      else{
+          $('.project-box').not('.'+value).hide('1000');
+          $('.project-box').filter('.'+value).show('1000');
+      }
+  });
+});
+
 
 
 
