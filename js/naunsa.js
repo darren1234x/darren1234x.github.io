05371a68497be69b5f70fe2ba998e4c16c98d883
darren1234x <114471474+darren1234x@users.noreply.github.com>
2022-12-30 17:09:03 +0800

    Initial commit

--- js/naunsa.js
+++ js/naunsa.js
@@ -5,20 +5,20 @@ var typed = new Typed(".typing", {
     loop: true
 });
 //accordion dropdown about
-var acc = document.getElementsByClassName("accordion-item");
-var i;
+const accItems = document.querySelectorAll('.accordion-item');
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
+accItems.forEach(item => {
+  item.addEventListener('click', function() {
+    this.classList.toggle('active');
+    const panel = this.nextElementSibling;
     if (panel.style.maxHeight) {
       panel.style.maxHeight = null;
     } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    } 
+      panel.style.maxHeight = panel.scrollHeight + 'px';
+    }
   });
-}
+});
+
 //Skills box
 const skillItems = document.querySelectorAll('.skill-item');
 
