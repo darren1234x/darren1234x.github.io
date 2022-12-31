56867ac5121fa57f1d951041046da5262cbee5fb
darren1234x <114471474+darren1234x@users.noreply.github.com>
2023-01-01 00:26:37 +0800

    Initial commit

---
+++ js/js.js
@@ -0,0 +1,57 @@
+var typed = new Typed(".typing", {
+    strings: [ "WEBSITE!", "PORTFOLIO!"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+//accordion dropdown about
+const accItems = document.querySelectorAll('.accordion-item');
+
+accItems.forEach(item => {
+  item.addEventListener('click', function() {
+    this.classList.toggle('active');
+    const panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + 'px';
+    }
+  });
+});
+//skills-carousel
+var swiper = new Swiper(".slide-content", {
+  slidesPerView: 3,
+  spaceBetween: 25,
+  loop: true,
+  centerSlide: 'true',
+  fade: 'true',
+  grabCursor: 'true',
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints:{
+      0: {
+          slidesPerView: 1,
+      },
+      520: {
+          slidesPerView: 2,
+      },
+      950: {
+          slidesPerView: 3,
+      },
+  },
+});
+
+
+
+
+
+
+
